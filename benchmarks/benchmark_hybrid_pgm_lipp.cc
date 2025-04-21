015836aa2b33013f1e4ba97b5148bc456c3a9389
sasank-chindirala <sasankchindirala@gmail.com>
2025-04-21 12:08:10 -0400

    - Modified benchmark_hybrid_pgm_lipp.cc to fix build error

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -1,9 +1,11 @@
 #include "benchmarks/benchmark_hybrid_pgm_lipp.h"
-
 #include "benchmark.h"
 #include "benchmarks/common.h"
 #include "competitors/hybrid_pgm_lipp.h"
 
+template <int record>
 void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
-  benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<uint64_t>, 8>>();
+  benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<record>, 8>>();
 }
+
+INSTANTIATE_TEMPLATES_MULTITHREAD(benchmark_64_hybrid_pgm_lipp, uint64_t);
