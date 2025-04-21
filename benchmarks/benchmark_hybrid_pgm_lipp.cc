3153a1761523d1158d67afe35ce53907d3aec005
sasank-chindirala <sasankchindirala@gmail.com>
2025-04-21 12:15:07 -0400

    - Modified benchmark_hybrid_pgm_lipp.cc to fix build error v2

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -3,9 +3,13 @@
 #include "benchmarks/common.h"
 #include "competitors/hybrid_pgm_lipp.h"
 
-template <int record>
-void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
-  benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<record>, 8>>();
+template <typename SearchClass, int record>
+void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark, bool pareto, const std::vector<int>& params) {
+  if (!pareto && params.empty()) {
+    benchmark.template Run<HybridPGMLIPP<uint64_t, SearchClass, 8>>();
+  } else {
+    util::fail("HybridPGMLIPP does not support pareto or parameter sweeps.");
+  }
 }
 
 INSTANTIATE_TEMPLATES_MULTITHREAD(benchmark_64_hybrid_pgm_lipp, uint64_t);
