cd8139ab9591dcae0c224ef065b09b76422cb20a
sasank-chindirala <sasankchindirala@gmail.com>
2025-04-21 12:30:56 -0400

    - Modified benchmark_hybrid_pgm_lipp.cc to fix build error v4

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -1,22 +1,9 @@
 #include "benchmarks/benchmark_hybrid_pgm_lipp.h"
+
 #include "benchmark.h"
 #include "benchmarks/common.h"
 #include "competitors/hybrid_pgm_lipp.h"
 
-template <typename SearchClass>
-void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark,
-                                  bool pareto, const std::vector<int>& params) {
-  if (!pareto && params.empty()) {
-    benchmark.template Run<HybridPGMLIPP<uint64_t, SearchClass, 8>>();
-  } else {
-    util::fail("HybridPGMLIPP does not support pareto tuning or custom params.");
-  }
+void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
+  benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<record>, 8>>();
 }
-
-template <int record>
-void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark,
-                                  const std::string& filename) {
-  benchmark.template Run<HybridPGMLIPP<uint64_t, BranchingBinarySearch<record>, 8>>();
-}
-
-INSTANTIATE_TEMPLATES_MULTITHREAD(benchmark_64_hybrid_pgm_lipp, uint64_t);
