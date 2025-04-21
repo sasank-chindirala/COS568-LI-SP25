82f04e18bae5d3344b6891fa5e18cad61caf2051
sasank-chindirala <sasankchindirala@gmail.com>
2025-04-21 12:40:48 -0400

    - Modified benchmark_hybrid_pgm_lipp.cc to fix build error v5

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -4,6 +4,18 @@
 #include "benchmarks/common.h"
 #include "competitors/hybrid_pgm_lipp.h"
 
-void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
+template <typename Searcher>
+void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark, bool pareto, const std::vector<int>& params) {
+  if (!pareto) {
+    util::fail("HybridPGMLIPP requires pareto mode with fixed hyperparameters");
+  } else {
+    benchmark.template Run<HybridPGMLIPP<uint64_t, Searcher, 8>>();
+  }
+}
+
+template <int record>
+void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark, const std::string& filename) {
   benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<record>, 8>>();
 }
+
+INSTANTIATE_TEMPLATES_MULTITHREAD(benchmark_64_hybrid_pgm_lipp, uint64_t);
