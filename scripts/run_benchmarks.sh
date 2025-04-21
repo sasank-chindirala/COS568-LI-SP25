461741b0754d20dbcb17d718056cf7b0c039a9d0
sasank-chindirala <sasankchindirala@gmail.com>
2025-04-21 11:43:53 -0400

    - Modified scripts to benchmark hybrid approach only on Facebook data

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -22,7 +22,7 @@ function execute_uint64_100M() {
 
 mkdir -p ./results
 
-for DATA in fb_100M_public_uint64 books_100M_public_uint64 osmc_100M_public_uint64
+for DATA in fb_100M_public_uint64
 do
 for INDEX in LIPP BTree DynamicPGM HybridPGMLIPP
 do
